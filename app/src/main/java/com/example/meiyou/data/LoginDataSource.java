7d384b63f874d2fc1cb6885f9609e4eaea36851a
MoebiusMeow <MoebiusMeow@qq.com>
2022-05-23 00:13:27 +0800

    Init commit

---
+++ app/src/main/java/com/example/meiyou/data/LoginDataSource.java
@@ -0,0 +1,31 @@
+package com.example.meiyou.data;
+
+import com.example.meiyou.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            return new Result.Error(new Exception("No Such User"));
+            /*
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);*/
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
